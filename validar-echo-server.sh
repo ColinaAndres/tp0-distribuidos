86c139591d67dbda6d059683b307cddb3429c346
Andres Colina <acolina@fi.uba.ar>
2026-03-14 14:53:08 -0300

    fix: change == simbol to = simbol to compare messages on the script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,7 +4,7 @@ TEST_MESSAGE="Testing Message from Script"
 
 SERVER_RESPONSE=$(docker run --rm --network tp0_testing_net busybox /bin/sh -c "echo '$TEST_MESSAGE' | nc server 12345")
 
-if [ "$SERVER_RESPONSE" == "$TEST_MESSAGE" ]; then
+if [ "$SERVER_RESPONSE" = "$TEST_MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
