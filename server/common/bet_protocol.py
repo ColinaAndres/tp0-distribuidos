9319687f04c7f4e0bbecb3eadc97db53ff45fbb4
Andres Colina <acolina@fi.uba.ar>
2026-03-18 14:57:08 -0300

    add: send_confirmation on protocol server to confirm reception of bet

--- server/common/bet_protocol.py
+++ server/common/bet_protocol.py
@@ -17,3 +17,9 @@ class BetProtocol:
         data = self._client_socket.recv(1024).decode('utf-8')
         agency, first_name, last_name, document, birthdate, number = data.split(',')
         return Bet(agency, first_name, last_name, document, birthdate, number)
+    
+    def send_confirmation(self)
+        """
+        Sends a confirmation message to the client socket.
+        """
+        self._client_socket.sendall(b'\x01')
