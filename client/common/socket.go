fe95ce37fade53e4bb8a9c47bc23e09862c7f610
Andres Colina <acolina@fi.uba.ar>
2026-03-19 10:09:14 -0300

    add: Documentation for socket.go

--- client/common/socket.go
+++ client/common/socket.go
@@ -2,10 +2,14 @@ package common
 
 import "net"
 
+// Socket is a wrapper around net.Conn that provides methods for sending and receiving data,
+// ensuring that all data is sent or received as needed.
 type Socket struct {
 	conn net.Conn
 }
 
+// NewSocket creates a new Socket instance by connecting to the specified server address.
+// It returns a pointer to the Socket and any error encountered during the connection process.
 func NewSocket(serverAddress string) (*Socket, error) {
 	conn, err := net.Dial("tcp", serverAddress)
 	if err != nil {
@@ -17,6 +21,9 @@ func NewSocket(serverAddress string) (*Socket, error) {
 
 }
 
+// SendAll sends the entire byte slice data to the connected server.
+// It ensures that all bytes are sent.
+// If an error occurs during sending, it returns the error.
 func (s *Socket) SendAll(data []byte) error {
 	totalSent := 0
 	for totalSent < len(data) {
@@ -29,6 +36,9 @@ func (s *Socket) SendAll(data []byte) error {
 	return nil
 }
 
+// ReceiveAll receives a specified amount of bytes from the connected server.
+// It ensures that the exact amount of bytes is received and returns the data as a byte slice.
+// If an error occurs during receiving, it returns the error.
 func (s *Socket) ReceiveAll(amountToReceive int) ([]byte, error) {
 	buffer := make([]byte, amountToReceive)
 	total_received := 0
@@ -42,6 +52,8 @@ func (s *Socket) ReceiveAll(amountToReceive int) ([]byte, error) {
 	return buffer, nil
 }
 
+// Close closes the connection to the server.
+// It returns any error encountered during the closing process.
 func (s *Socket) Close() error {
 	return s.conn.Close()
 }
