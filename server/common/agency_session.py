9bbe8e81a5757554fb82df9ed5e418042f415c29
Andres Colina <acolina@fi.uba.ar>
2026-03-21 15:20:11 -0300

    add: receive_request encapsulation on agencysession

--- server/common/agency_session.py
+++ server/common/agency_session.py
@@ -1,5 +1,16 @@
 
+from common.command import Command
+
+
 class AgencySession:
+    """
+    Class that represents a session with an agency
+    """
     def __init__(self, agency_id, protocol):
         self._agency_id = agency_id
         self._protocol = protocol
+
+    def receive_request(self) -> Command:
+        """Receives a request from protocol."""
+        return self._protocol.receive_request()
+    
