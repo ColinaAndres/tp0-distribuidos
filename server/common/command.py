8039a92f6abc780dcfffdaca2fdd5e96160ae85c
Andres Colina <acolina@fi.uba.ar>
2026-03-22 20:56:51 -0300

    refactor: Commands return False unless it is a FInalizationCommand

--- server/common/command.py
+++ server/common/command.py
@@ -1,7 +1,7 @@
 
 class Command:
     """Base class for commands that can be executed on the server."""
-    def execute(self, _server, _agency):
+    def execute(self, _lottery_central, _agency) -> bool:
         pass
 
 class BetsProcessingCommand(Command):
@@ -11,16 +11,20 @@ class BetsProcessingCommand(Command):
         self._bets = bets
         self._agency_id = bets[0].agency if bets else None
 
-    def execute(self, server, agency):
+    def execute(self, lottery_central, agency) -> bool:
         agency.agency_id = self._agency_id
-        server.process_bets(self._bets, agency)
+        lottery_central.process_bets(self._bets, agency)
+        return False
 
 class FinalizationCommand(Command):
-    """Command to indicate the finalization of the bet sending."""
-    def execute(self, server, _agency):
-        server.finalize_reception_of_bets()
+    """Command to indicate the finalization of the bet sending. returns 
+    True to indicate that the session should be closed."""
+    def execute(self, lottery_central, _agency) -> bool:
+        lottery_central.register_agency_done()
+        return True
 
 class WinnersRequestCommand(Command):
     """Command to request the winners of the bet."""
-    def execute(self, server, agency):
-        server.send_winners(agency)
+    def execute(self, lottery_central, agency) -> bool:
+        lottery_central.send_winners(agency)
+        return False
