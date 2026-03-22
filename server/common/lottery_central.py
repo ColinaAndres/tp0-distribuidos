e63c9134c7a70ff5673edf67f7d5814260408e93
Andres Colina <acolina@fi.uba.ar>
2026-03-22 20:38:51 -0300

    add: documentation for lottery_central class

--- server/common/lottery_central.py
+++ server/common/lottery_central.py
@@ -11,21 +11,37 @@ class Lottery_central:
         self._winners = []
 
     def process_bets(self, bets):
+        """
+        Process the bets received from an agency, storing them and logging the action
+        """
         store_bets(bets)
         logging.info(f"action: apuesta_recibida | result: success | cantidad: {len(bets)}")
 
     def register_agency_done(self):
+        """
+        Register that an agency has finished sending bets, 
+        if all agencies are done, runs the lottery
+        """
         self._done_agencies += 1
         logging.info(f"action: finalizacion_recepcion_apuestas | result: success")
         if self._done_agencies == self._total_agencies:
             self._run_lottery()
 
     def draw_done(self) -> bool:
+        """
+        Check if the lottery draw is done
+        """
         return True if self._winners else False
 
     def get_winners_for_agency(self, agency_id) -> list:
+        """
+        Get the winners for a specific agency id
+        """
         return list(filter(lambda bet: bet.agency == agency_id, self._winners))
 
     def _run_lottery(self):
+        """
+        Run the lottery, storing the winners and logging the action
+        """
         self._winners = list(filter(has_won, load_bets()))
         logging.info(f"action: sorteo | result: success | ganadores: {len(self._winners)}")
