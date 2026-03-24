5f4df1043eb84a3e1f85857b018fad0ea9d96906
Andres Colina <acolina@fi.uba.ar>
2026-03-24 13:29:38 -0300

    add: OSError catch on socket to throw custom conection error

--- server/common/socket.py
+++ server/common/socket.py
@@ -10,15 +10,18 @@ class Socket:
         If the connection is closed before receiving all data, 
         raises a ConnectionError.
         '''
-        received_data = b''
-        while len(received_data) < amount_to_receive:
-            chunk = self._skt.recv(amount_to_receive - len(received_data))
-            if not chunk and not received_data:
-                return None
-            if not chunk:
-                raise ConnectionError("Socket connection closed before receiving all data")    
-            received_data += chunk
-        return received_data
+        try:
+            received_data = b''
+            while len(received_data) < amount_to_receive:
+                chunk = self._skt.recv(amount_to_receive - len(received_data))
+                if not chunk and not received_data:
+                    return None
+                if not chunk:
+                    raise ConnectionError("Socket connection closed before receiving all data")    
+                received_data += chunk
+            return received_data
+        except OSError:
+            raise ConnectionError("Socket was manually closed during receive")
     
     def send_all(self, data):
         """
